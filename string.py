03a5a6b8c099f0f71b3f87fe669192658f816f42
Jamshid <itsjk0307>
2024-03-28 16:19:10 +0900

    doing some review

--- string.py
+++ string.py
@@ -6,9 +6,35 @@ Created on Thu Mar 28 15:22:58 2024
 """
 
 # f_string function
-
-
 name='Jamshid'
 surname="Khaytbaev"
 full_name=f"{name} {surname}"
 print(full_name)
+print("Hello World!")
+print("Hello \tWorld! ")
+print('Hello \nWorld!')
+
+
+
+# upper() va lower() metodlari
+# title() va capitalize() metodlari
+# strip(), rstrip() va lstrip() metodlari
+
+name="Jamshid"
+surname="Khaytbaev"
+text=" I love you "
+full_name=f" {name} {surname}"
+print(full_name.upper())
+print(full_name.lower())
+print(full_name.title())
+print(full_name.capitalize())
+print(text.strip())
+print(text.lstrip())
+print(text.rstrip())
+
+
+
+# INPUT —FOYDALANUVCHI BILAN MULOQOT
+
+name=input("What is your name?")
+print("Hello " + name)
