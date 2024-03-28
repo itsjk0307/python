664258cd1a424db7101ba39ba2c29c689fc744da
Jamshid <itsjk0307>
2024-03-28 15:26:09 +0900

    .

---
+++ string.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 28 15:22:58 2024
+
+@author: MyHome
+"""
+
+# f_string function
+
+
+name='Jamshid'
+surname="Khaytbaev"
+full_name=f"{name} {surname}"
+print(full_name)
