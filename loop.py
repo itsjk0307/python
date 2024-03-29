02d3ab98fe5db4394f3e5051783032bfdedbd808
Jamshid <itsjk0307>
2024-03-29 10:52:41 +0900

    '.'

---
+++ loop.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 29 10:24:21 2024
+
+@author: MyHome
+"""
+
+# mehmonlar = ['Ali','Vali','Hasan', 'Husan','Olim']
+# for mehmon in mehmonlar:
+#     print(mehmon)
+    
+    
+#     mehmonlar = ['Ali','Vali','Hasan', 'Husan','Olim']
+# for mehmon in mehmonlar:
+#     print(f"Hurmatli {mehmon}, sizni 20 Dekabr kuni nahorga oshga taklif qilamiz")
+#     print("Hurmat bilan, Palonchiyevlar oilasi")
+    
+    
+# # for YORDAMIDA SONLI RO'YXATLAR BILAN ISHLASH
+# sonlar = list(range(1,11))
+# for son in sonlar:
+#     print(f"{son} ning kvadrati {son**2} ga teng")
+    
+# sonlar = list(range(11)) # 1 dan 10 gacha sonlar ro'yxatini yaratamiz
+# sonlar_kvadrati =[] # bo'sh ro'yxat yaratamiz
+# for son in sonlar:  # sonlar dagi har bir son uchun
+#     sonlar_kvadrati.append(son**2) # uning kv.ni hisoblab, sonlar_kvadrati ga yuklaymiz
+
+# print(sonlar)
+# print(sonlar_kvadrati)
+
+
+# friends=[]
+# for n in  range(5):
+#     friends.append(input(f"Enter {n+1}-friend's name: "))
+# print(friends)
+
+
+friends=['Jamshid', 'Babur', 'Sukhrab ', 'Merdan ', 'Zamira ']
+for friend in friends:
+    print(f"Salom {friend}")
+print(f"Kod {len(friends)} marta takrorlandi")
