6177b8460a9ce95af6d78d94f757de48bdc715d0
Jamshid <itsjk0307>
2024-03-29 11:03:33 +0900

    .

---
+++ conditions.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 29 11:01:52 2024
+
+@author: MyHome
+"""
+avtolar = ['audi','bmw','volvo','kia','hyundai']
+for avto in avtolar: # avtolar ichidadi har bir avto uchun ...
+    if avto == 'bmw':  # ... agar avto bmw ga teng bo'lsa ...
+        print(avto.upper()) # avto nomini hamma harflarini katta bilan yoz.
+    else: # aks holda ... 
+        print(avto.title()) # avto nomini faqat birinchi harfini katta bilann yoz.
